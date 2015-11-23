85f58abb5b4edcf02e31a5e8d3648751cf87ccf6
Simon Monecke <SimonMonecke@gmail.com>
2015-11-23 16:26:24 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject lambdacd-testng "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
- :license {:name "Apache License, Version 2.0"
+  :description "Clojure Library to display TestNG reports at the top of a LambdaCD step"
+  :url "https://github.com/otto-de/lambdacd-testng"
+  :scm {:name "git"
+        :url "https://github.com/otto-de/lambdacd-testng.git"}
+  :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.rpl/specter "0.8.0"]
